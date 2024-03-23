4c0ed1598f9dd7b9ccef1c079607be418a01e356
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-23 12:25:49 -0300

    feat: initial files

---
+++ src/main/java/io/github/Ital023/CourseAPISpringBoot/CourseApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.CourseAPISpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Ital023/CourseAPISpringBoot/CourseApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.CourseAPISpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
